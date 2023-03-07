4bd5ba434517178e7c39a53a01f41bf6ad54a4a0
Leeco <leeco19@qq.com>
2023-03-07 16:53:42 +0800

    7-6

---
+++ src/bin/7-6.rs
@@ -0,0 +1,4 @@
+// 使用文件系统的模块
+mod demo_module;
+
+fn main() {}
---
+++ src/bin/demo_module.rs
@@ -0,0 +1,7 @@
+pub mod hosting;
+
+fn main() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
