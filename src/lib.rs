4dac48e3b9f206af6964be979bbe4a55790b09f6
Leeco <leeco19@qq.com>
2023-03-17 14:42:33 +0800

    12-6

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents =
     // ? 运算符会返回错误值给调用者
         fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     Ok(())
 }
 
@@ -29,5 +30,35 @@ impl Config {
         let filename = args[2].clone();
 
         Ok(Config { query, filename })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+// TDD 测试驱动开发
+// 测试指令: cargo test --lib
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
