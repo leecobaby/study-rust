914a919bcbc807145270deee35b196dfd2e798a2
Leeco <leeco19@qq.com>
2023-03-22 14:23:48 +0800

    14-1

---
+++ src/bin/14-1.rs
@@ -0,0 +1,8 @@
+// Cargo
+// release profile - 预定义的配置文件
+// dev profile - cargo build
+// release profile - cargo build --release
+
+// 自定义 profile
+// 在 Cargo.toml 中添加 [profile.dev] 或 [profile.release] 部分
+fn main() {}
