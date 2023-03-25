56e94a53af352358a945f607563c7bb14f971e17
Leeco <leeco19@qq.com>
2023-03-25 20:29:37 +0800

    14-6

---
+++ src/bin/14-6.rs
@@ -0,0 +1,10 @@
+// 从 crates.io 安装 crate
+// cargo install cargo-edit
+// cargo add [crate_name]
+// cargo add [crate_name] --vers [crate_version]
+// cargo add [crate_name] --git [crate_git_url]
+
+// 限制：只能安装二进制文件
+
+// 使用自定义命令扩展 cargo
+// 如果 $HOME/.cargo/bin 在 $PATH 中，有个二进制文件叫做 cargo-xxx，那么就可以通过 cargo xxx 来运行它
