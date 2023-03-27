8572f00b39507771cd38ff76452196b0d762d926
Leeco <leeco19@qq.com>
2023-03-27 16:55:15 +0800

    15-3

--- src/bin/15-3.rs
+++ src/bin/15-3.rs
@@ -1,36 +1,26 @@
-// 函数和方法的隐式解引用转化 Deref coercion
-// 假设 T 实现了 Deref trait，那么 &T 将会自动转化为 &U，其中 U 是 T 实现 Deref trait 返回的类型的引用。
-use std::ops::Deref;
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
+// Drop Trait
+// 实现 Drop Trait 使我们可以自定义当值离开作用域时的行为
+struct CustomSmartPointer {
+    data: String,
 }
 
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        &self.0
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
     }
-}
-
-fn hello(name: &str) {
-    println!("Hello, {}!", name);
 }
 
 fn main() {
-    let m = MyBox::new(String::from("Rust"));
-
-    // &m => &MyBox<String> => deref(&String) => &String => deref(&str) => &str
-    hello(&m);
-    // hello(&(*m)[..]);
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created.");
+    // 不允许手动调用 drop
+    // c.drop(c);
+    // 但可以调用 std::mem::drop 函数，它在预导入模块里
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main.");
 }
-
-// 解引用和可变性
-// 可以使用 DerefMut trait 重载可变引用的 * 运算符
-// 当 T: Deref<Target=U> 时，&T 可以被转化为 &U
-// 当 T: DerefMut<Target=U> 时，&mut T 可以被转化为 &mut U
-// 当 T: Deref<Target=U> 时，&mut T 可以被转化为 &U
