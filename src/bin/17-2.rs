c96cc8066000d2447b4a0e0f5b9ed84d09225d8e
Leeco <leeco19@qq.com>
2023-04-03 18:20:51 +0800

    17-2

---
+++ src/bin/17-2.rs
@@ -0,0 +1,64 @@
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    // dyn 意思是这样实现了 Draw trait 就能放在里面
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!("draw button");
+    }
+}
+
+
+pub struct SelectBox {
+    pub width: u32,
+    pub height: u32,
+    pub options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("draw selectbox");
+    }
+}
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
