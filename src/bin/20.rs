0ee7ec785f9fc24583f6cf08e03f897ea9432bda
Leeco <leeco19@qq.com>
2023-04-09 16:08:31 +0800

    20-1

---
+++ src/bin/20.rs
@@ -0,0 +1,55 @@
+// 构建多线程 Web 服务器
+// - 在 socket 上监听 TCP 连接
+// - 解析少量的 HTTP 请求
+// - 创建一个合适的 HTTP 响应
+// - 使用线程池改进服务器的吞吐量
+
+use std::{
+    fs,
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        // println!("Connection established!");
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+
+    // 请求
+    // Method Request-URI HTTP-Version CRLF
+    // headers CRLF
+    // message-body
+
+    // 响应
+    // HTTP-Version Status-Code Reason-Phrase CRLF
+    // headers CRLF
+    // message-body
+
+    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let response = format!("{}{}", status_line, contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+}
